b38592b0e74ce31d4ff62344960119528aaffb1a
weizhiqiang <598748873@qq.com>
2024-09-01 21:08:16 +0800

    feat: skyeye-note合入skyeye-adm

--- skyeye-adm/adm-pro/src/main/java/com/skyeye/eve/folder/service/impl/FolderServiceImpl.java
+++ skyeye-adm/adm-pro/src/main/java/com/skyeye/eve/folder/service/impl/FolderServiceImpl.java
@@ -9,7 +9,6 @@ import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.skyeye.annotation.service.SkyeyeService;
 import com.skyeye.base.business.service.impl.SkyeyeBusinessServiceImpl;
 import com.skyeye.common.constans.CommonConstants;
-import com.skyeye.common.constans.NoteConstants;
 import com.skyeye.common.enumeration.DeleteFlagEnum;
 import com.skyeye.common.object.InputObject;
 import com.skyeye.common.object.OutputObject;
@@ -20,6 +19,7 @@ import com.skyeye.eve.folder.dao.FolderDao;
 import com.skyeye.eve.folder.entity.Folder;
 import com.skyeye.eve.folder.service.FolderService;
 import com.skyeye.exception.CustomException;
+import com.skyeye.util.NoteConstants;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
