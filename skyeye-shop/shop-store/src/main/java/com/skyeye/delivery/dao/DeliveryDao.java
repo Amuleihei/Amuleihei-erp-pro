226812acc957624ef199e7534daa548dacbf906e
SUCH HAVE YOU <2207141659@qq.com>
2024-09-28 20:07:55 +0800

    feat.删除多余文件

--- skyeye-shop/shop-store/src/main/java/com/skyeye/delivery/dao/DeliveryDao.java
+++
@@ -1,8 +0,0 @@
-package com.skyeye.delivery.dao;
-
-
-import com.skyeye.delivery.entity.Delivery;
-import com.skyeye.eve.dao.SkyeyeBaseMapper;
-
-public interface DeliveryDao  extends SkyeyeBaseMapper<Delivery> {
-}
