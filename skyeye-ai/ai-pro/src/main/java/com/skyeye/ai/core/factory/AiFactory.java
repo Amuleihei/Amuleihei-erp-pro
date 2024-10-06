99e13809c8241306564ea1d603f97958d3e8c74e
sdhkjh <2450473462@qq.com>
2024-10-06 17:24:57 +0800

    feat: 修复提交失败

---
+++ skyeye-ai/ai-pro/src/main/java/com/skyeye/ai/core/factory/AiFactory.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright 卫志强 QQ：598748873@qq.com Inc. All rights reserved. 开源地址：https://gitee.com/doc_wei01/skyeye
+ ******************************************************************************/
+
+package com.skyeye.ai.core.factory;
+
+import com.skyeye.ai.core.enums.AiPlatformEnum;
+
+/**
+ * @ClassName: AiFactory
+ * @Description: AI Model 模型工厂的接口类
+ * @author: skyeye云系列--卫志强
+ * @date: 2024/10/5 11:33
+ * @Copyright: 2024 https://gitee.com/doc_wei01/skyeye Inc. All rights reserved.
+ * 注意：本内容仅限购买后使用.禁止私自外泄以及用于其他的商业目的
+ */
+public interface AiFactory {
+
+    /**
+     * 基于指定配置，获得 ChatModel 对象
+     * <p>
+     * 如果不存在，则进行创建
+     *
+     * @param platform 平台
+     * @param apiKey   API KEY
+     * @param url      API URL
+     * @return ChatModel 对象
+     */
+    Object getOrCreateChatModel(AiPlatformEnum platform, String apiKey, String secretKey, String url);
+
+    /**
+     * 基于默认配置，获得 ChatModel 对象
+     * <p>
+     * 默认配置，指的是在 application.yaml 配置文件中的 spring.ai 相关的配置
+     *
+     * @param platform 平台
+     * @return ChatModel 对象
+     */
+    Object getDefaultChatModel(AiPlatformEnum platform);
+
+    /**
+     * 基于默认配置，获得 ImageModel 对象
+     * <p>
+     * 默认配置，指的是在 application.yaml 配置文件中的 spring.ai 相关的配置
+     *
+     * @param platform 平台
+     * @return ImageModel 对象
+     */
+    Object getDefaultImageModel(AiPlatformEnum platform);
+
+    /**
+     * 基于指定配置，获得 ImageModel 对象
+     * <p>
+     * 如果不存在，则进行创建
+     *
+     * @param platform 平台
+     * @param apiKey   API KEY
+     * @param url      API URL
+     * @return ImageModel 对象
+     */
+    Object getOrCreateImageModel(AiPlatformEnum platform, String apiKey, String url);
+
+}
---
+++ skyeye-ai/ai-pro/src/main/java/com/skyeye/message/controller/MessageController.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright 卫志强 QQ：598748873@qq.com Inc. All rights reserved. 开源地址：https://gitee.com/doc_wei01/skyeye
+ ******************************************************************************/
+
+package com.skyeye.message.controller;
+
+import com.skyeye.annotation.api.Api;
+import com.skyeye.annotation.api.ApiImplicitParam;
+import com.skyeye.annotation.api.ApiImplicitParams;
+import com.skyeye.annotation.api.ApiOperation;
+import com.skyeye.common.object.InputObject;
+import com.skyeye.common.object.OutputObject;
+import com.skyeye.message.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: MessageController
+ * @Description: 消息控制层
+ * @author: skyeye云系列--卫志强
+ * @date: 2024/10/5 17:24
+ * @Copyright: 2024 https://gitee.com/doc_wei01/skyeye Inc. All rights reserved.
+ * 注意：本内容仅限购买后使用.禁止私自外泄以及用于其他的商业目的
+ */
+@RestController
+@Api(value = "消息管理", tags = "消息管理", modelName = "消息管理")
+public class MessageController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @ApiOperation(id = "sendMessage", value = "发送消息", method = "POST", allUse = "0")
+    @ApiImplicitParams({
+        @ApiImplicitParam(id = "message", name = "message", value = "消息", required = "required")})
+    @RequestMapping("/post/MessageController/sendMessage")
+    public void sendMessage(InputObject inputObject, OutputObject outputObject) {
+        messageService.sendMessage(inputObject, outputObject);
+    }
+
+}
