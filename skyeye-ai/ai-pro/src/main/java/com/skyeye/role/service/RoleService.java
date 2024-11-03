265423252c2df203ef79e343df4c742c2b0c4180
SUCH HAVE YOU <2207141659@qq.com>
2024-11-03 21:11:04 +0800

    feat.修改ai配置和角色代码

--- skyeye-ai/ai-pro/src/main/java/com/skyeye/role/service/RoleService.java
+++ skyeye-ai/ai-pro/src/main/java/com/skyeye/role/service/RoleService.java
@@ -18,5 +18,5 @@ import java.util.List;
  * 注意：本内容仅限购买后使用.禁止私自外泄以及用于其他的商业目的
  */
 public interface RoleService extends SkyeyeBusinessService<Role> {
-    List<Role> queryRoleList(String id);
+    
 }
